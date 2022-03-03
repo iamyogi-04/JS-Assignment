22f590eb5346b41d3d8efa82fe00644680f7e0b5
iamyogi-04 <jyogesh977@gmail.com>
2022-03-03 12:21:06 +0530

    Can drive assignment

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,11 +2,11 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
-    if (operation=='add'){
+    if (operation==="add"){
         movies.push(movie)
 
     }
-    if (operation=='remove'){
+    if (operation==="remove"){
         movies.pop()
     }
     return movies
